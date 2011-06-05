dadbad3de2c0e2ee66105b9b03e75221837a2aa5
Chris Broadfoot <cbro@google.com>
2011-06-05 12:58:46 +1000

    wire up geolocation button

--- ushahidi/embed/js/geolocation.js
+++ ushahidi/embed/js/geolocation.js
@@ -2,8 +2,8 @@ ushahidi.Geolocation = function(map) {
   this.map_ = map.getMap();
   this.location_ = this.map_.getCenter();
   this.zoomLevel_ = 12
+
   this.marker_ = new google.maps.Marker({
-    map: this.map_,
     icon: new google.maps.MarkerImage(
       'http://oa-samples.googlecode.com/svn-history/r73/trunk/' +
         'presentations/gdd-2010/saopaulo/talks/maps/my-location.png',
@@ -17,36 +17,24 @@ ushahidi.Geolocation = function(map) {
 
 ushahidi.Geolocation.prototype.updateLocation = function() {
   var me = this;
-  this.browserSupportFlag_ = false;
-  if(navigator.geolocation) {
-    me.browserSupportFlag_ = true;
-    navigator.geolocation.getCurrentPosition(function(position) {
-      me.location_ = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-      me.accuracy_ = position.coords.accuracy;
-      me.updateMap_();
-    }, function() {
-      me.handleNoGeolocation_();
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    browserSupportFlag = false;
-    me.handleNoGeolocation_(browserSupportFlag);
-  }
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var location = new google.maps.LatLng(position.coords.latitude,
+                                          position.coords.longitude);
+    var accuracy = position.coords.accuracy;
+    me.updateMap_(location, accuracy);
+  });
 };
 
-ushahidi.Geolocation.prototype.handleNoGeolocation_ = function() {
-  // noop
-};
-
-ushahidi.Geolocation.prototype.updateMap_ = function() {
+ushahidi.Geolocation.prototype.updateMap_ = function(location, accuracy) {
   var map = this.map_;
-  map.setCenter(this.location_);
+  map.panTo(location);
   map.setZoom(this.zoomLevel_);
 
-  this.marker_.setPosition(this.location_);
+  this.marker_.setPosition(location);
+  this.marker_.setMap(map);
 
   this.circle_.setOptions({
-    radius: this.accuracy_,
+    radius: accuracy,
     strokeColor: '#44c4ff',
     fillColor: '#44c4ff'
   });
