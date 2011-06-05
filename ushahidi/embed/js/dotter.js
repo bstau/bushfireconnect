0e1bceb69e66a887ba03a4f237d3f32754d19a7f
Chris Broadfoot <cbro@google.com>
2011-06-05 13:35:24 +1000

    dotter

---
+++ ushahidi/embed/js/dotter.js
@@ -0,0 +1,27 @@
+ushahidi = ushahidi || {};
+
+ushahidi.Dotter = function(width, height) {
+  this.width_ = width;
+  this.height_ = height;
+  this.cache = {};
+};
+
+ushahidi.Dotter.prototype.getDot = function(color) {
+  if (!this.cache[color]) {
+    this.cache[color] = this.generateDot_(color);
+  }
+
+  return this.cache[color];
+};
+
+ushahidi.Dotter.prototype.generateDot_ = function(color) {
+  var el = document.createElement('canvas');
+  el.width = this.width_;
+  el.height = this.height_;
+
+  var ctx = el.getContext('2d');
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, el.width, el.height);
+
+  return el.toDataURL();
+};
--- ushahidi/embed/js/layers.js
+++ ushahidi/embed/js/layers.js
@@ -1,14 +1,23 @@
 // TODO: some way to customise and filter the markers
 // TODO: modal dialog instead of info window
-ushahidi.IncidentOverlay = function(api) {
+ushahidi.IncidentOverlay = function(api, dotter) {
   this.api_ = api;
+  this.dotter_ = dotter;
 };
 
 ushahidi.IncidentOverlay.prototype.setMap = function(map) {
   var me = this;
   me.map_ = map;
-  me.api_.getIncidents(function(resp) {
-    me.setIncidents_(resp.payload.incidents);
+  me.api_.getCategories(function(resp) {
+    var catsA = resp.payload.categories;
+    var cats = {};
+    for (var i = 0; i < catsA.length; i++) {
+      var cat = catsA[i].category;
+      cats[cat.id] = cat;
+    }
+    me.api_.getIncidents(function(resp) {
+      me.setIncidents_(resp.payload.incidents, cats);
+    });
   });
   var infowindow = new google.maps.InfoWindow;
   google.maps.event.addListener(map, 'incident_changed', function() {
@@ -24,17 +33,20 @@ ushahidi.IncidentOverlay.prototype.setMap = function(map) {
   });
 };
 
-ushahidi.IncidentOverlay.prototype.setIncidents_ = function(incidents) {
+ushahidi.IncidentOverlay.prototype.setIncidents_ = function(incidents, cats) {
   var map = this.map_;
 
   for (var i = 0, ii = incidents.length; i < ii; i++) {
     var incident = incidents[i].incident;
+    var category = incidents[i].categories[0].category;
     var loc = new google.maps.LatLng(incident.locationlatitude,
         incident.locationlongitude);
 
+    var color = '#' + cats[category.id].color;
     var marker = new google.maps.Marker({
       position: loc,
-      map: map
+      map: map,
+      icon: this.dotter_.getDot(color)
     });
     marker.incident_ = incidents[i];
 
